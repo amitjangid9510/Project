dbe265a67ff116962cdf99b5fffee5d585720627
Amit jangid <56021794+amitjangid9510@users.noreply.github.com>
2024-04-16 14:28:46 +0530

    Update multer.middelware.js

--- project/src/middelware/multer.middelware.js
+++ project/src/middelware/multer.middelware.js
@@ -3,7 +3,7 @@ const os = require('os');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'os.tmpdir()');
+    cb(null, os.tmpdir());
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
