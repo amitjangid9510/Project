bf50265b178078d4ad53c12147310bd1c2b6f979
Amit jangid <56021794+amitjangid9510@users.noreply.github.com>
2024-04-22 14:52:15 +0530

    Update app.js

--- project/app.js
+++ project/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: "*",
     credentials : true
 }))
 app.use(express.json({limit : "16kb"}))
